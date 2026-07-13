d832942a7538d41e907617108f7700387cab0bf4
Aditya Jadhav <a.jadhav.work@gmail.com>
2026-07-13 12:40:10 +0500

    Add task creation feature with controlled input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 function App() {
+  const [tasks , setTasks] = useState([
+
+    "learn React",
+    "Build TaskFlow ",
+    "practice Js"
+
+  ]);
+
+  const [newTask, setNewTask ] = useState("");
+
+  function addTask(){
+    if (newTask.trim() === "") return;
+
+    setTasks([...tasks , newTask]);
+
+    setNewTask("");
+  }
+
   return (
-    <h1>TaskFlow - Organize. Focus. Achieve.</h1>
+    <div>
+      <h1>TaskFlow</h1>
+      
+      <input 
+      type="text"
+      placeholder="enter the task..."
+      value = {newTask}
+      onChange={(event) => setNewTask(event.target.value)}
+      />
+
+      <button onClick={addTask}> Add </button>
+      <ul>
+        {tasks.map((task,index) => (
+          <li key={index} > {task} </li>
+        ))}
+        </ul>
+      </div>
   );
 }
 
-export default App;
+  export default App;
